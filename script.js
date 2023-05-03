d94350ef31f5b5282dcf05ba3e6257af517a7433
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-03 14:22:32 +0200

    exercice 3, prompt and display time in future

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-import { offsetZone } from "./exercice1.js";
-import { HowManyDays } from "./exercice2.js";
+// import { offsetZone } from "./exercice1.js";
+// import { HowManyDays } from "./exercice2.js";
+import { FutureTime } from "./exercice3.js";
 
-offsetZone();
-HowManyDays();
+// offsetZone();
+// HowManyDays();
+FutureTime();
